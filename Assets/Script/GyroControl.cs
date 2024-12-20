da0591941e04e4c955a248796075ede9710911c3
Knocknock_0820 <steven82036@gmail.com>
2024-12-21 03:28:45 +0800

    Fix gyro

--- Assets/Script/GyroControl.cs
+++ Assets/Script/GyroControl.cs
@@ -1,37 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
-// using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class GyroControl : MonoBehaviour
 {
     private bool gyroEnabled;
-    [SerializeField] private Vector3 rotationChange;
-    [SerializeField] private float gyro_sensity;
+    private Quaternion initialRotation; // To store the initial gyroscope rotation
     public GameObject player;
 
     void Start()
     {
-        if(SystemInfo.supportsGyroscope)
+        if (SystemInfo.supportsGyroscope)
         {
             Input.gyro.enabled = true;
-        }else{
-            Debug.Log("gyro not enabled, please retry");
+            initialRotation = new Quaternion(Input.gyro.attitude.x, Input.gyro.attitude.z, Input.gyro.attitude.y, Input.gyro.attitude.w);
+        }
+        else
+        {
+            Debug.Log("Gyroscope not supported, please retry on a compatible device.");
         }
     }
 
     void Update()
     {
-        if(SystemInfo.supportsGyroscope && Input.gyro.enabled == false)
+        if(Keyboard.current.kKey.wasPressedThisFrame){
+            initialRotation = new Quaternion(Input.gyro.attitude.x, Input.gyro.attitude.z, Input.gyro.attitude.y, Input.gyro.attitude.w);
+            Debug.Log("Reset gyroscope.");
+        }
+
+        if (SystemInfo.supportsGyroscope && !Input.gyro.enabled)
         {
-            Debug.Log("Reenabled gyroscope");
+            initialRotation = new Quaternion(Input.gyro.attitude.x, Input.gyro.attitude.z, Input.gyro.attitude.y, Input.gyro.attitude.w);
+            Debug.Log("Re-enabled gyroscope.");
             Input.gyro.enabled = true;
         }
     }
 
-    void FixedUpdate(){
-        transform.position = player.GetComponent<Transform>().position;
-        transform.rotation = new Quaternion(Input.gyro.attitude.x, Input.gyro.attitude.z, Input.gyro.attitude.y, -Input.gyro.attitude.w);
+    void FixedUpdate()
+    {
+        // Synchronize the object with the player's position
+        transform.position = player.transform.position;
+
+        // Adjust rotation based on the initial calibration
+        Quaternion currentRotation = new Quaternion(Input.gyro.attitude.x, Input.gyro.attitude.z, Input.gyro.attitude.y, Input.gyro.attitude.w);
+        Quaternion adjustedRotation = Quaternion.Inverse(initialRotation) * currentRotation;
+
+        // Apply the adjusted rotation directly
+        transform.rotation = adjustedRotation;
     }
 }
